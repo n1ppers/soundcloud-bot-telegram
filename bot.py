3794fc9eeb5e248ccdedb4d0a52c69ae17e03370
n1ppers <specialforgames251@gmail.com>
2019-05-03 17:29:16 +0300

    stable

---
+++ bot.py
@@ -0,0 +1,60 @@
+import telebot
+import os
+import re
+
+from sclib import SoundcloudAPI, Track, Playlist
+from telebot import types
+
+TOKEN = os.environ["TOKEN"]
+
+bot = telebot.TeleBot(TOKEN)
+api = SoundcloudAPI()
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.send_message(message.from_user.id, 
+        '''
+Commands: \n/github - Get a link to repository with my source code on GitHub \n/support - Contact my owner
+/track - Download single track by link
+        ''')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    print(message.from_user.username, ": ", message.text)
+    if message.text == "/github":
+        command_github(message)
+    elif message.text == "/support":
+        command_support(message)
+    elif message.text == "/track":
+        command_track(message)
+    else:
+        bot.send_message(message.from_user.id, "I don't understand you. If you want to get a list of commands - type /help.")
+
+def command_github(message):
+    bot.send_message(message.from_user.id, "GitHub \nhttps://github.com/n1ppers/soundcloud-bot")
+
+def command_support(message):
+    bot.send_message(message.from_user.id, "Telegram: @n1ppers")
+
+def command_track(message):
+    msg = bot.send_message(message.from_user.id, "Please, send me a link to track that you want to download.")
+    bot.register_next_step_handler(msg, download_track)
+
+def download_track(message):
+    if message.text.startswith("https://soundcloud.com/"):
+        track = api.resolve(message.text)
+        assert type(track) is Track
+        name = f'./{track.artist} - {track.title}.mp3'
+        filename = re.sub('[\|/|:|*|?|"|<|>\|]', '', name)
+        with open(filename, 'wb+') as fp:
+            try:
+                track.write_mp3_to(fp)
+                audio = open(filename, 'rb')
+                bot.send_audio(message.from_user.id, audio)
+                os.remove(filename)
+            except (FileNotFoundError):
+                bot.send_message(message.from_user.id, "An error has occured while downloading this track. Please, tell @n1ppers about this error.")
+    else:
+        bot.send_message(message.from_user.id, "Oops, looks like, that's not a SoundCloud link.")
+
+bot.polling(none_stop=True, interval=0)
