c2abefd4749db235bf92310c110fad213a60a885
n1ppers <specialforgames251@gmail.com>
2019-05-03 17:32:00 +0300

    dev

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def send_welcome(message):
 	bot.send_message(message.from_user.id, 
         '''
 Commands: \n/github - Get a link to repository with my source code on GitHub \n/support - Contact my owner
-/track - Download single track by link
+/track - Download single track by link \n/playlist - Download all tracks from playlist by link
         ''')
 
 @bot.message_handler(content_types=['text'])
@@ -27,6 +27,8 @@ def get_text_messages(message):
         command_support(message)
     elif message.text == "/track":
         command_track(message)
+    elif message.text == "/playlist":
+        command_playlist(message)
     else:
         bot.send_message(message.from_user.id, "I don't understand you. If you want to get a list of commands - type /help.")
 
@@ -39,6 +41,10 @@ def command_support(message):
 def command_track(message):
     msg = bot.send_message(message.from_user.id, "Please, send me a link to track that you want to download.")
     bot.register_next_step_handler(msg, download_track)
+
+def command_playlist(message):
+    msg = bot.send_message(message.from_user.id, "Please, send me a link to playlist that you want to download.")
+    bot.register_next_step_handler(msg, download_playlist)
 
 def download_track(message):
     if message.text.startswith("https://soundcloud.com/"):
@@ -54,6 +60,24 @@ def download_track(message):
                 os.remove(filename)
             except (FileNotFoundError):
                 bot.send_message(message.from_user.id, "An error has occured while downloading this track. Please, tell @n1ppers about this error.")
+    else:
+        bot.send_message(message.from_user.id, "Oops, looks like, that's not a SoundCloud link.")
+
+def download_playlist(message):
+    if message.text.startswith("https://soundcloud.com/"):
+        playlist = api.resolve(message.text)
+        assert type(playlist) is Playlist
+        for track in playlist.tracks:
+            name = f'./{track.artist} - {track.title}.mp3'
+            filename = re.sub('[\|/|:|*|?|"|<|>\|]', '', name)
+            with open(filename, 'wb+') as fp:
+                try:
+                    track.write_mp3_to(fp)
+                    audio = open(filename, 'rb')
+                    bot.send_audio(message.from_user.id, audio)
+                    os.remove(filename)
+                except (FileNotFoundError):
+                    bot.send_message(message.from_user.id, "An error has occured while downloading this track. Please, tell @n1ppers about this error.")
     else:
         bot.send_message(message.from_user.id, "Oops, looks like, that's not a SoundCloud link.")
 
