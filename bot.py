1018634ede9641e7372744d514fa5e116ccc6e74
OwO#8196 <specialforgames251@gmail.com>
2019-08-16 17:18:58 +0300

    Update bot.py
    try and catch for bot.polling

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import re
 import os.path
 import sys
+import time
 
 from sclib import SoundcloudAPI, Track, Playlist
 from telebot import types
@@ -87,12 +88,15 @@ def isLinkValid(link):
         return True
     if link.startswith("http://soundcloud.com/"):
         return True
+    if link.startswith("soundcloud.com/"):
+        return True
     if link.startswith("http://m.soundcloud.com/"):
         return True
     if link.startswith("https://m.soundcloud.com/"):
         return True
+    if link.startswith("m.soundcloud.com/"):
+        return True
     
-    print("Link is not valid")
     return False
 
 def getURL(msg):
@@ -100,10 +104,14 @@ def getURL(msg):
     if not msg.startswith("https://soundcloud.com/"):
         if msg.startswith("http://soundcloud.com/"):
             url = msg.replace("http://soundcloud.com/", "https://soundcloud.com/")
+        elif msg.startswith("soundcloud.com/"):
+            url = msg.replace("soundcloud.com/", "https://soundcloud.com/")
         elif msg.startswith("http://m.soundcloud.com/"):
             url = msg.replace("http://m.soundcloud.com/", "https://soundcloud.com/")
         elif msg.startswith("https://m.soundcloud.com/"):
             url = msg.replace("https://m.soundcloud.com/", "https://soundcloud.com/")
+        elif msg.startswith("m.soundcloud.com/"):
+            url = msg.replace("m.soundcloud.com/", "https://soundcloud.com/")
         else:
             url = msg
     
@@ -153,4 +161,15 @@ def download_playlist(message):
             bot.send_message(message.from_user.id, f"An error has occured while downloading {track.artist} - {track.title}")
             continue
 
-bot.polling(none_stop=True, interval=0)
+counter = int(0)
+while True:
+    if counter >= 9: 
+        print("Cannot call bot.polling after 10 attempts. \n")
+        sys.exit()
+    try:
+        bot.polling(none_stop=True, interval=0)
+        counter = 0
+    except Exception as ex:
+        counter += 1
+        print(f"An error has occured while calling bot.polling. Exception below. \n \n %s" % str(ex))
+        time.sleep(5)
